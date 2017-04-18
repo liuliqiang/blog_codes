df339e4bd95c09925de523dc403bc933d934f31e
Liqiang Liu <liqianglau@outlook.com>
2017-04-18 09:31:57 +0800

    Add: multiprocess demo part01

---
+++ multiprocess_demo/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+"""
+    A simple python script template.
+    Created by yetship at 2017/4/18 上午8:47
+"""
+
+import sys
+
+
+def main(arguments):
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
