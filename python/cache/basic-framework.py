f3fafbb0ca3550ac67069ab6776f0f855f1284d3
Liqiang Liu <liqianglau@outlook.com>
2017-08-28 17:39:15 +0800

    Add: cache blog code

---
+++ python/cache/basic-framework.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import time
+
+
+class BackingStore:
+    def __init__(self):
+        self.data = []
+
+    def write(self, datum):
+        print('Started writing to backing store.')
+        time.sleep(2)  # Writing to disk is slow
+        self.data.append(datum)
+        print('Finished writing to backing store.')
+
+    def read(self, index):
+        print('Started reading from backing store.')
+        time.sleep(2)  # Reading from disk is slow
+        print('Finished reading from backing store.')
+        return self.data[index]
+
+
+class Cache:
+    def __init__(self):
+        self.data = []
+
+    def write(self, datum):
+        print('Started writing to cache.')
+        self.data.append(datum)
+        print('Finished writing to cache.')
+
+    def read(self, index):
+        print('Started reading from backing store.')
+        print('Finished reading from backing store.')
+        return self.data[index]
