de5ab9ecc834a16738532a2d8798d6a6e21fee06
tyrael lau <liqianglau@outlook.com>
2017-09-05 12:12:57 +0800

    Mod: real runable test

--- cpp/python-src/main.cpp
+++ cpp/python-src/main.cpp
@@ -1,14 +1,29 @@
-#include <iostream>
-
 #include "Python.h"
+
+#include <iostream>
+using namespace std;
+
 
 int main()
 {
+    // !!! Important
+    Py_Initialize();
+
     PyObject *t = PyTuple_New(3);
     PyTuple_SetItem(t, 0, PyLong_FromLong(1L));
     PyTuple_SetItem(t, 1, PyLong_FromLong(2L));
     PyTuple_SetItem(t, 2, PyUnicode_FromString("three"));
 
-    PyObject_Print(t, stdout, 0);
+    cout << "size of PyTupleObject: " << sizeof(*t) << endl;
+
+    PyObject *num1 = PyLong_FromLong(3L);
+    PyObject_Print(num1, stdout, Py_PRINT_RAW);
+    cout << endl;
+
+    PyObject_Print(t, stdout, Py_PRINT_RAW);
+    cout << endl;
+
+    // !!! Important too
+    Py_Finalize();
     return 0;
 }
